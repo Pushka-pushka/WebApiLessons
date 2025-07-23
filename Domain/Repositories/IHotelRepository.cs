bb501958f4a692afdb6a22e65b51b76c743ab857
Pushka-pushka <admirallPushkin@yandex.ru>
2025-07-24 02:52:52 +0300

    запросы можно делатть через Swagger

--- Domain/Repositories/IHotelRepository.cs
+++ Domain/Repositories/IHotelRepository.cs
@@ -10,6 +10,7 @@ namespace HotelsWebApi.Domain.Repositories
     public interface IHotelRepository : IDisposable
     {
         Task <List<Hotel>> GetHotelsAsync();
+        Task <List<Hotel>> GetHotelsAsync(string name);
         Task <Hotel> GetHotelAsync(int hotelId);
         Task InsertHotelAsync(Hotel hotel);
         Task UpdateHotelAsync(Hotel hotel);
